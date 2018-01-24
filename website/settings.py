405f9da15d97430060ff1e6fab04c355b7025d87
3VRM <admin@3vrm.com>
2018-01-24 12:17:01 +0000

    update gunicorn

--- website/settings.py
+++ website/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'home/templates',
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
